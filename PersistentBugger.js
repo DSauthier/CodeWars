4eb3a33b5c64a2750aaf85a6093bf5d537fc2ac5
DSauthier <danielsauthier@live.com>
2018-11-27 09:29:24 -0500

    Persistent Bugger Challenge added

---
+++ PersistentBugger.js
@@ -0,0 +1,36 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+
+// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+// // and 4 has only one digit
+
+// persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+// // 1*2*6 = 12, and finally 1*2 = 2
+
+// persistence(4) === 0 // because 4 is already a one-digit number
+
+function persistence(number) {
+  if (number.toString().length < 2) {
+    return 0;
+  }
+
+  let numberArray = number.toString().split('');
+  let theProduct = numberArray.reduce((accumulator, current) => {
+    return accumulator * current
+  })
+  let howManyTimes = 1;
+
+
+  while (theProduct.toString().length > 1) {
+    numberArray = theProduct.toString().split('');
+    theProduct = numberArray.reduce((accumulator, current) => {
+      return accumulator * current
+    })
+    howManyTimes++
+  }
+  return howManyTimes
+}
+
+
+persistence(39)
