6a7aa90d3fd70cd13220728b1d29c32f02be2732
DSauthier <danielsauthier@live.com>
2019-01-13 11:19:25 -0500

    time conversion comented

--- timeConversion.js
+++ timeConversion.js
@@ -1,8 +1,14 @@
 function timeConversion(s) {
-  const arr = s.slice(0, 8).split(':');
+  const arr = s.slice(0, s.length - 2).split(':');
+  console.log(arr)
+  // create a new array where the PM will be removed split the string by :
+  console.log(arr[0])
+  // (s.indexOf('PM') > -1) ? means -> if the index that generates the string PM is in the string (exists) do something. if it doesnt, keep going.
   arr[0] = (s.indexOf('PM') > -1) ?
+    // if arr[0] = 12 return 12 else return the number+12 ; if arr[0] = 0 return 0 else arr[0]
     (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
     (arr[0] == 12 ? '00' : arr[0]);
+  // now all we need to do is join the parts together
   return arr.join(':');
 }
 
